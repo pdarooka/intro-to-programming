3a499609f93373d6c94bc8d741902d9b62a7cc76
pd <pd@PDs-iMac.local>
2020-07-06 06:24:59 +0530

    Increased efficiency with ranges

--- src/For_Loops/Binary_Hex_Converter.py
+++ src/For_Loops/Binary_Hex_Converter.py
@@ -5,28 +5,25 @@ print("Welcome!")
 
 # Gets user input
 num_queries = int(input("How many values would you like to convert from binary to hexadecimal?\n"))
-decimal = []
-binary = []
-hexadecimal = []
-
-for x in range(1, num_queries + 1):
-    decimal.append(x)
+decimal = list(range(1, num_queries+1))
+binary = [bin(x) for x in decimal]
+hexadecimal = [hex(x) for x in decimal]
 
 low_slice = int(input("What decimal number would you like to start at:\n"))
 hi_slice = int(input("What decimal number would you like to stop at:\n"))
 
 # Prints the lists
 print(f"Decimal values from {low_slice} to {hi_slice}")
-for y in range(low_slice, hi_slice + 1):
+for y in decimal[low_slice:hi_slice+1]:
     print(str(y))
 
 print(f"Binary values from {low_slice} to {hi_slice}")
-for y in range(low_slice, hi_slice + 1):
-    print(str(bin(y)))
+for y in binary[low_slice:hi_slice+1]:
+    print(str(y))
 
 print(f"Hexadecimal values from {low_slice} to {hi_slice}")
-for y in range(low_slice, hi_slice + 1):
-    print(str(hex(y)))
+for y in hexadecimal[low_slice:hi_slice+1]:
+    print(str(y))
 
 input(f"Press Enter to see all values from 1 to {num_queries}.\n")
 print("Decimal----Binary----Hexadecimal\n----------------------------------------------------------")
